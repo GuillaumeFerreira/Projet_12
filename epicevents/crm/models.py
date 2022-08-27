4b47c0a04aa0a9521864c4c82118e218dd319464
ferreira <guillaumeferreira00@gmail.com>
2022-08-27 17:18:00 +0200

    created models

--- epicevents/crm/models.py
+++ epicevents/crm/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 
 class Team(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -8,49 +8,49 @@ class Team(models.Model):
 
 class Employee(models.Model):
     id = models.BigAutoField(primary_key=True)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
-    mobile = models.CharField(max_length=20)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now_add=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, default=1)
+    first_name = models.CharField(max_length=25, default=" ")
+    last_name = models.CharField(max_length=25, default=" ")
+    email = models.CharField(max_length=100, default=" ")
+    phone = models.CharField(max_length=20, default=" ")
+    mobile = models.CharField(max_length=20, default=" ")
+    date_created = models.DateTimeField(default=datetime.datetime.now())
+    date_update = models.DateTimeField(default=datetime.datetime.now())
 
 class Client(models.Model):
     id = models.BigAutoField(primary_key=True)
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
-    mobile = models.CharField(max_length=20)
-    company_name = models.CharField(max_length=250)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now_add=True)
-    employee_contact = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25, default=" ")
+    last_name = models.CharField(max_length=25, default=" ")
+    email = models.CharField(max_length=100, default=" ")
+    phone = models.CharField(max_length=20, default=" ")
+    mobile = models.CharField(max_length=20, default=" ")
+    company_name = models.CharField(max_length=250, default=" ")
+    date_created = models.DateTimeField(default=datetime.datetime.now())
+    date_update = models.DateTimeField(default=datetime.datetime.now())
+    employee_contact = models.ForeignKey(Employee, on_delete=models.CASCADE, default=1)
 
 
 class Contract(models.Model):
     id = models.BigAutoField(primary_key=True)
-    employee_contact = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    client = models.OneToOneField(Client, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now_add=True)
-    status = models.BooleanField()
-    amount = models.FloatField()
-    payment_due = models.DateTimeField(auto_now_add=True)
+    employee_contact = models.ForeignKey(Employee, on_delete=models.CASCADE, default=1)
+    client = models.OneToOneField(Client, on_delete=models.CASCADE, default=1)
+    date_created = models.DateTimeField( default=datetime.datetime.now())
+    date_update = models.DateTimeField(default=datetime.datetime.now())
+    status = models.BooleanField(default=False)
+    amount = models.FloatField(default=10.2)
+    payment_due = models.DateTimeField(default=datetime.datetime.now())
 
 
 class Event(models.Model):
     id = models.BigAutoField(primary_key=True)
-    client = models.OneToOneField(Client, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now_add=True)
-    employee_contact = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="sales_contact")
-    employee_event = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="event_contact")
-    event_date = models.DateTimeField(auto_now_add=True)
+    client = models.OneToOneField(Client, on_delete=models.CASCADE, default=1)
+    date_created = models.DateTimeField(default=datetime.datetime.now())
+    date_update = models.DateTimeField(default=datetime.datetime.now())
+    employee_contact = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="sales_contact", default=1)
+    employee_event = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="event_contact", default=1)
+    event_date = models.DateTimeField(default=datetime.datetime.now())
     notes = models.TextField(max_length=8192, blank=True)
-    attendees = models.IntegerField()
+    attendees = models.IntegerField(default=200)
 
 
 
