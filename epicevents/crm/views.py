4e89b821cffe2fe0432eef55da91976067c244b5
ferreira <guillaumeferreira00@gmail.com>
2022-08-30 08:52:09 +0200

    application des permissions

--- epicevents/crm/views.py
+++ epicevents/crm/views.py
@@ -1,24 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from . import models, serializers
+from . import models, serializers, permissions
 
 
 class ClientsViewset(ModelViewSet):
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = (IsAuthenticated, permissions.ClientPermissions)
     serializer_class = serializers.ClientsSerializer
     queryset = models.Client.objects.all()
 
 
 class ContractViewset(ModelViewSet):
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = (IsAuthenticated, permissions.ContractPermissions)
     serializer_class = serializers.ContractSerializer
     queryset = models.Contract.objects.all()
 
 
 class EventViewset(ModelViewSet):
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = (IsAuthenticated, permissions.EventPermissions )
     serializer_class = serializers.EventSerializer
     queryset = models.Event.objects.all()
