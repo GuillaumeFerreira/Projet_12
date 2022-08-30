e849f7741ed6d31973e4582567e1ccd7af5e703e
ferreira <guillaumeferreira00@gmail.com>
2022-08-30 17:30:04 +0200

    Update admin.py

--- epicevents/crm/admin.py
+++ epicevents/crm/admin.py
@@ -1,13 +1,43 @@
 from django.contrib import admin
 from . import models
-
+from django.urls import reverse
 
 
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('id',  'notes', 'client', 'employee_event')
+    def has_change_permission(self, request):
+        if request.user.role == "SUPPORT":
+            return True
+        else:
+            return False
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(employee_event=request.user)
+
+class ContractAdmin(admin.ModelAdmin):
+
+    def has_change_permission(self, request):
+
+        if request.user.role == "COMMERCIAL":
+            return True
+        else:
+            return False
+
+    def has_module_permission(self,request):
+        try:
+            if request.user.role == "COMMERCIAL":
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(employee_contact=request.user)
 
 admin.site.register(models.Client)
-admin.site.register(models.Contract)
+admin.site.register(models.Contract,ContractAdmin)
 admin.site.register(models.Event,EventAdmin)
 admin.site.register(models.Employee)
