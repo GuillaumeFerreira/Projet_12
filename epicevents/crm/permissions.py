c69f22afb8ee7ea00fa9fe18e550a19a8a8e62cf
ferreira <guillaumeferreira00@gmail.com>
2022-08-31 16:38:49 +0200

    Update permissions.py

--- epicevents/crm/permissions.py
+++ epicevents/crm/permissions.py
@@ -1,24 +1,46 @@
 from rest_framework import permissions
-
+from . import models
 
 class ClientPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         # Utilisation de permissions.SAFE_METHODS, is a tuple containing 'GET', 'OPTIONS' and 'HEAD'
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.role == "COMMERCIAL"
+        else:
+            if request.user.role in ["MANAGER","COMMERCIAL"]:
+                if request.method in permissions.SAFE_METHODS:
+                    return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user == obj.employee_contact
+            if request.user.role == "SUPPORT":
+                event = models.Event.objects.filter(employee_event=request.user)
+                return obj == event.client
+            if request.user.role == "COMMERCIAL":
+                return request.user == obj.employee_contact
+            else:
+                return True
+        elif request.method == "POST":
+            if request.user.role == "SUPPORT":
+                return False
+            else:
+                return True
+        else:
+            if request.user.role == "COMMERCIAL":
+                return request.user == obj.employee_contact
+            elif request.user.role == "SUPPORT":
+                return False
+            else:
+                return True
+
+
 
 
 class ContractPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        elif request.method in ("PUT", "PATCH"):
+        elif request.method in ["PUT", "PATCH"]:
             return request.user.role == "COMMERCIAL"
 
     def has_object_permission(self, request, view, obj):
