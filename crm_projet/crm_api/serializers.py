03c09ac3d1c5b9561a96058c4d9aea83cdf6fc90
ferreira <guillaumeferreira00@gmail.com>
2022-09-05 07:32:02 +0200

    Create serializers.py

---
+++ crm_projet/crm_api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer
+from . import models
+
+
+class ClientsSerializer(ModelSerializer):
+    class Meta:
+        model = models.Client
+        fields = "__all__"
+
+
+class ContractSerializer(ModelSerializer):
+    class Meta:
+        model = models.Contract
+        fields = "__all__"
+
+
+class EventSerializer(ModelSerializer):
+    class Meta:
+        model = models.Event
+        fields = "__all__"
