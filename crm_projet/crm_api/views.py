ca231683e985c69a154a2519d9842cbf2c526a3b
ferreira <guillaumeferreira00@gmail.com>
2022-09-05 07:34:24 +0200

    Update views.py

--- crm_projet/crm_api/views.py
+++ crm_projet/crm_api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from . import models, serializers
 
-# Create your views here.
+class ClientsViewset(ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = serializers.ClientsSerializer
+    queryset = models.Client.objects.all()
+
+
+
+class ContractViewset(ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = serializers.ContractSerializer
+    queryset = models.Contract.objects.all()
+
+
+class EventViewset(ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = serializers.EventSerializer
+    queryset = models.Event.objects.all()
