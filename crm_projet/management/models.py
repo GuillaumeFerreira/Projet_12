76519af8c1c7ee52db9c1eee21c4b879df38b4ef
ferreira <guillaumeferreira00@gmail.com>
2022-09-14 10:20:16 +0200

    correction password

--- crm_projet/management/models.py
+++ crm_projet/management/models.py
@@ -1,7 +1,15 @@
-from django.db import models
+from django.db import models , transaction
 from django.contrib.auth.models import AbstractUser
 
 
 class Employee(AbstractUser):
 
     phone = models.CharField(max_length=128, blank=True)
+
+    def save(self, *args, **kwargs):
+        user = super(Employee, self)
+
+        if len(user.password) != 88:
+            user.set_password(self.password)
+        user.save()
+        return user
